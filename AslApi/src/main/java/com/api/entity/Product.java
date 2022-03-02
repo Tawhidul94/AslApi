0be2e2a292546ad43444ed4efd7b087d19bdf499
Tawhidul94 <islamtawhidul910@gmail.com>
2022-03-02 11:45:08 +0600

    entity class completed

--- AslApi/src/main/java/com/api/entity/Product.java
+++ AslApi/src/main/java/com/api/entity/Product.java
@@ -1,8 +1,38 @@
 package com.api.entity;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "product")
 public class Product {
 	
+	@Id
+	@Basic(optional = false)
 	private String productId;
+	
+	@Basic(optional = false)
 	private String ProductName;
+	
+	private String productBrand;
+	
+	private String productCatagory;
+	
+	private int qunataty;
+	
+	private BigDecimal price;
+	
+	private String note;
+	
+	@Transient
+	private BigDecimal totalAmount;
 
 }
